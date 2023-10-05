f3d5892e5ef95825c57a0c112ca49534058e31c1
Erfan <Erfan@sth.com>
2023-10-05 22:19:49 +0330

    slight change

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function loadItems(){
-    
+const URL = "https://erfansefat.github.io/BlankSpace/items.json";
+const container = document.getElementById("item-container");
+async function loadData(){
+    const items = await fetch(URL);
+    const data = items.json();
+    return data;
+}
+async function loadItems(){
+    const items = await loadData();
+    items.vinyls.forEach(element => {
+        const card = document.createElement("div");
+        card.className = "item-card";
+        container.appendChild(card);
+
+        const name = document.createElement("div");
+        name.className = "item-name";
+        name.textContent = element.item_name;
+        card.appendChild(name);
+    });
 }
 loadItems();
