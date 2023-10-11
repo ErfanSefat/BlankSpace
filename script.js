5c15d6f640938f22fe818dfea533a34b95473a27
Erfan <Erfan@sth.com>
2023-10-11 19:36:12 +0330

    filters are working now

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 const URL = "https://erfansefat.github.io/BlankSpace/items.json";
 const container = document.getElementById("item-container");
+const removeBtn = document.getElementById("remove-filters");
 async function loadData(){
     const items = await fetch(URL);
     const data = items.json();
     return data;
 }
-async function loadItems(){
+async function getItems(){
     const items = await loadData();
     items.vinyls.forEach(element => {
-        const card = document.createElement("div");
-        card.className = "item-card";
-        container.appendChild(card);
-
-        const img = document.createElement("img");
-        img.className = "item-img";
-        img.src = `./Pictures/vinyls/${element.item_img}`;
-        card.appendChild(img);
-
-        const name = document.createElement("div");
-        name.className = "item-name";
-        name.textContent = element.item_name;
-        card.appendChild(name);
-
-        const info = document.createElement("div");
-        info.className = "item-info";
-        card.appendChild(info);
-        const release = document.createElement("div");
-        release.className = "item-release";
-        release.textContent = element.release_year;
-        const type = document.createElement("div");
-        type.className = "item-type";
-        type.textContent = element.type;
-
-        const distance1 = document.createElement("img");
-        distance1.src = "./Pictures/dis.svg";
-        const distance2 = document.createElement("img");
-        distance2.src = "./Pictures/dis.svg";
-
-        const genre = document.createElement("div");
-        genre.className = "item-genre";
-        genre.textContent = element.genre;
-
-        info.appendChild(release);
-        info.appendChild(distance1);
-        info.appendChild(genre);
-        info.appendChild(distance2);
-        info.appendChild(type);
-
-        const pricebox = document.createElement("div");
-        pricebox.className = "item-price";
-        card.appendChild(pricebox);
-
-        const carting = document.createElement("div");
-        carting.className = "add-to-cart";
-        carting.textContent = "Add to Cart";
-
-        const price = document.createElement("div");
-        price.className = "price";
-        price.textContent = `$${element.item_price}`;
-
-        pricebox.appendChild(carting);
-        pricebox.appendChild(price);
-
+        loadItem(element);
     });    
+}
+function loadItem(element){
+    const card = document.createElement("div");
+    card.className = "item-card";
+    container.appendChild(card);
+
+    const img = document.createElement("img");
+    img.className = "item-img";
+    img.src = `./Pictures/vinyls/${element.item_img}`;
+    card.appendChild(img);
+
+    const name = document.createElement("div");
+    name.className = "item-name";
+    name.textContent = element.item_name;
+    card.appendChild(name);
+
+    const info = document.createElement("div");
+    info.className = "item-info";
+    card.appendChild(info);
+    const release = document.createElement("div");
+    release.className = "item-release";
+    release.textContent = element.release_year;
+    const type = document.createElement("div");
+    type.className = "item-type";
+    type.textContent = element.type;
+
+    const distance1 = document.createElement("img");
+    distance1.src = "./Pictures/dis.svg";
+    const distance2 = document.createElement("img");
+    distance2.src = "./Pictures/dis.svg";
+
+    const genre = document.createElement("div");
+    genre.className = "item-genre";
+    genre.textContent = element.genre;
+
+    info.appendChild(release);
+    info.appendChild(distance1);
+    info.appendChild(genre);
+    info.appendChild(distance2);
+    info.appendChild(type);
+
+    const pricebox = document.createElement("div");
+    pricebox.className = "item-price";
+    card.appendChild(pricebox);
+
+    const carting = document.createElement("div");
+    carting.className = "add-to-cart";
+    carting.textContent = "Add to Cart";
+
+    const price = document.createElement("div");
+    price.className = "price";
+    price.textContent = `$${element.item_price}`;
+
+    pricebox.appendChild(carting);
+    pricebox.appendChild(price);
+
 }
 async function loadToMenuBoxes()
 {
@@ -88,20 +92,70 @@ async function loadToMenuBoxes()
         const date = document.createElement("div");
         date.className = "menu-options";
         date.textContent = element;
+        date.addEventListener("click", function(){
+            filterDate(element)
+        });
         menuRelease.appendChild(date);
     });
     typeList.forEach(element => {
         const date = document.createElement("div");
         date.className = "menu-options";
         date.textContent = element;
+        date.addEventListener("click", function(){
+            filterType(element)
+        });
         menuType.appendChild(date);
     });
     genreList.forEach(element => {
         const date = document.createElement("div");
         date.className = "menu-options";
         date.textContent = element;
+        date.addEventListener("click", function(){
+            filterGenre(element)
+        });
         menuGenre.appendChild(date);
     });
 }
-loadItems();
 loadToMenuBoxes();
+getItems();
+async function removeCards(){
+    const cards = await document.querySelectorAll(".item-card");
+    cards.forEach(element => {
+        element.remove();
+    });
+}
+async function filterGenre(whatToFilter){
+    const items = await loadData();
+    removeCards();
+    items.vinyls.forEach(element => {
+        if(element.genre === whatToFilter){
+            loadItem(element);
+        }
+    });
+    removeBtn.style.visibility = "visible";
+}
+async function filterDate(whatToFilter){
+    const items = await loadData();
+    removeCards();
+    items.vinyls.forEach(element => {
+        if(element.release_year === whatToFilter){
+            loadItem(element);
+        }
+    });
+    removeBtn.style.visibility = "visible";
+}
+async function filterType(whatToFilter){
+    const items = await loadData();
+    removeCards();
+    items.vinyls.forEach(element => {
+        if(element.type === whatToFilter){
+            loadItem(element);
+        }
+    });
+    removeBtn.style.visibility = "visible";
+}
+function hideBtn(){
+    removeCards();
+    getItems();
+    removeBtn.style.visibility = "hidden";
+}
